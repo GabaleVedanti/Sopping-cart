110bbad0aff32b643c016838c9a32700cc9bf8b0
GabaleVedanti <165984999+GabaleVedanti@users.noreply.github.com>
2024-04-12 16:45:28 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "shopping cart";  
+    $conn = mysqli_connect($servername, $username, $password, $db_name,3307) or die("Connection failed: " . mysqli_connect_error());
+?>
---
+++ header.php
@@ -0,0 +1,10 @@
+<header class="header">
+    <div class ="header_body">
+        <a href="index.php" class="logo">Amazon</a>
+        <a href="index.php">Add Product</a>
+        <a href="view_products.php">View Products</a>
+        <a href="">Shopit</a>
+        
+        <a href="shopping_cart.php" class="cart"><i class="fas fa-shopping-cart"></i><span><sup>4</sup></span></a>
+    </div>
+</header>
